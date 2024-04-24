b10249d97a3963954200f5f5a5103b663975b3c2
Riki Fujihara <rikifujihara@gmail.com>
2024-04-24 10:16:52 +1000

    save

---
+++ index.js
@@ -0,0 +1,11 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+// server setup
+const server = new ApolloServer();
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
+
+console.log("server ready at port: ", 4000);
